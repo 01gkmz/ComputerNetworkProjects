c7b2a4bd2acc738cab6ffa9dddfe614d5aff2e29
01gkmz <sxl2199@case.edu>
2024-02-06 16:30:13 -0500

    ok le

--- project1/echo_server.py
+++ project1/echo_server.py
@@ -8,37 +8,31 @@ BUF_SIZE = 4096
 def main():
     print("----- Echo Server -----")
     try:
-        serverSock = socket.socket()
+        server_socket = socket.socket()
     except socket.error as err:
         print("socket creation failed with error %s" % (err))
         exit(1)
-    serverSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serverSock.bind(('192.168.16.1', ECHO_PORT))
-    serverSock.listen(5)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('127.0.0.1', ECHO_PORT))
+    server_socket.listen(5)
 
-    fd_map = {serverSock.fileno(): serverSock}
+    fd_map = {server_socket.fileno(): server_socket}
 
-    pollerObject = select.poll()
-    pollerObject.register(serverSock, select.POLLIN)
+    poller = select.poll()
+    poller.register(server_socket, select.POLLIN)
 
     while True:
-        events = pollerObject.poll()
+        events = poller.poll()
         for fd, event in events:
-            if fd == serverSock.fileno():
-                client, addr = serverSock.accept()
+            if fd == server_socket.fileno():
+                client, addr = server_socket.accept()
                 print("Connection from ", addr)
-                pollerObject.register(client, select.POLLIN | select.POLLERR)
+                poller.register(client, select.POLLIN | select.POLLERR)
                 fd_map[client.fileno()] = client
 
             elif event & select.POLLIN:
-                data = fd_map[fd].recv(1024).decode()
-                if not data:
-                    pollerObject.unregister(fd)
-                    fd_map[fd].close()
-                    del fd_map[fd]
-                    continue
-                print("client: ", data)
-                fd_map[fd].send(data.encode())
+                data = fd_map[fd].recv(1024)
+                fd_map[fd].send(data)
 
 
 if __name__ == '__main__':
